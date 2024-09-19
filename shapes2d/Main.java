bd30af63e243a6a4879517f91c76757244651a66
Prismtask <161811156+Prismtask@users.noreply.github.com>
2024-09-19 12:37:31 +0700

    ok

--- shapes2d/Main.java
+++ shapes2d/Main.java
@@ -1,6 +1,9 @@
 package shapes2d;
 
 import polygon.interfaces.Geom2D;
+import polygon.types.Circle;
+import polygon.types.Rectangle;
+import polygon.types.RightTriangle;
 import polygon.types.Square;
 
 import javax.swing.JFrame;
@@ -10,10 +13,10 @@ public class Main {
 
 	protected static InstanceData instanceData = new InstanceData();
 	public static void main(String[] args) {
-		init();
+		UserInterface.getUserInput();
 	}
 
-	private static void init() {
+	protected static void init() {
 		JFrame fr = new JFrame();
 		fr.setBounds(50, 50, 450, 500);
 		fr.setDefaultCloseOperation(3);
@@ -21,21 +24,38 @@ public class Main {
 		instanceData.newWorld();
 
 		for (int i = 0; i < 100; i++) {
-			instanceData.selectedWorld.addShape(new Square((int) (Math.random() * 50 + 50)), (int) (Math.random() * 1000), (int) (Math.random() * 1000));
+			instanceData.selectedWorld.addShape(new Circle((int) (Math.random() * 50 + 50)), (int) (Math.random() * 1000), (int) (Math.random() * 1000));
 		}
 
 		fr.add(instanceData.selectedWorld.drawer);
 		fr.setVisible(true);
 
-		instanceData.newWorld();
+		JFrame fr2 = new JFrame();
+		fr2.setBounds(50, 50, 450, 500);
+		fr2.setDefaultCloseOperation(3);
 
+		instanceData.newWorld();
 		instanceData.selectWorld(1);
 
 		for (int i = 0; i < 100; i++) {
-			instanceData.selectedWorld.addShape(new Square((int) (Math.random() * 50 + 50)), (int) (Math.random() * 1000), (int) (Math.random() * 1000));
+			instanceData.selectedWorld.addShape(new Rectangle((int) (Math.random() * 50 + 50), (int) (Math.random() * 50 + 50)), (int) (Math.random() * 1000), (int) (Math.random() * 1000));
 		}
 
-		fr.add(instanceData.selectedWorld.drawer);
-		fr.setVisible(true);
+		fr2.add(instanceData.selectedWorld.drawer);
+		fr2.setVisible(true);
+
+		JFrame fr3 = new JFrame();
+		fr3.setBounds(50, 50, 450, 500);
+		fr3.setDefaultCloseOperation(3);
+
+		instanceData.newWorld();
+		instanceData.selectWorld(2);
+
+		for (int i = 0; i < 100; i++) {
+			instanceData.selectedWorld.addShape(new RightTriangle((int) (Math.random() * 50 + 50), (int) (Math.random() * 50 + 50)), (int) (Math.random() * 1000), (int) (Math.random() * 1000));
+		}
+
+		fr3.add(instanceData.selectedWorld.drawer);
+		fr3.setVisible(true);
 	}
 }
